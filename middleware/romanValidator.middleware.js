723a453b23f4e83994cc2b78beabc654ddd2c093
Tony George <tg764562@gmail.com>
2025-07-12 17:20:53 +0300

    feat: decimal to roman

--- middleware/romanValidator.middleware.js
+++ middleware/romanValidator.middleware.js
@@ -1,6 +1,6 @@
 function romanValidator(req, res, next) {
   const question = req.body.question;
-  if (!question || (isNaN(parseInt(question)) && question.match(/[^IVXLCDM]/g) !== null)){
+  if (!question || (typeof question == 'string' && question.match(/[^IVXLCDM]/g) !== null) || question <= 0 || question >= 4000){
     return res.status(400).json({ error: 'question input not valid' });
   }
   next();
--- services/roman.service.js
+++ services/roman.service.js
@@ -21,9 +21,35 @@ function romanConverter(roman) {
 
   return total;
 }
+
+function decimalConverter(decimal) {
+  const romanNumerals = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
+  let result = '';
+  for (const [roman, value] of romanNumerals) {
+    while (decimal >= value) {
+      result += roman;
+      decimal -= value;
+    }
+  }
+  return result;
+}
   
 async function convert(question) {
-  const answer = typeof question == 'string'? romanConverter(question): 'from decimal to roman is not implemented yet';
+  const answer = typeof question == 'string'? romanConverter(question): decimalConverter(question);
   const solution = new Solution({question: question, answer})
   await solution.save();
   return solution;
